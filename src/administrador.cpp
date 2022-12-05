40a38212b5f497339ed41410dfa443405e4ea462
i12hurel <i12hurel@uco.es>
2022-12-05 21:13:57 +0100

    commit porsi acaso (clase listaesperay admin añadida)

---
+++ src/administrador.cpp
@@ -0,0 +1,18 @@
+/*
+ * administrador.cpp
+ *
+ *  Created on: 5 dic 2022
+ *      Author: Lau
+ */
+
+#include "administrador.h"
+
+administrador::administrador() {
+	// TODO Auto-generated constructor stub
+
+}
+
+administrador::~administrador() {
+	// TODO Auto-generated destructor stub
+}
+
