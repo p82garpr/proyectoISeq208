dbae7160d99c1ff6e83e7f621d0f9d851e044b14
rafag <rafag@rafag>
2022-12-11 19:26:37 +0100

    main actualizado

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,14 @@
 // Description : Main menu of our fancy program
 //============================================================================
 
-/*
-#ifdef _wIN32
+
+#ifdef _WIN32
 #define CLEAR "cls"
 #elif  defined(unix)||defined(__unix__)||defined(__unix)||defined(__APPLE__)||defined(__MACH__)
 #define CLEAR "clear"
+#error "EL sistema no acepta CLEAR como orden para limpiar pantalla"
 #endif
-*/
+
 
 #include <iostream>
 #include <string>
@@ -22,10 +23,11 @@
 #include "usuario.h"
 #include "curso.h"
 #include "alumno.h"
+//#include "visitante.h"
+
 using namespace std;
 
 void enter();
-int menu_inicio(string &dni,string &pass);
 void menu_admin();
 void menu_visitante();
 void menu_alumno(string &dni);
@@ -33,46 +35,13 @@ void menu_alumno(string &dni);
 int main() {
 	curso c;
 	alumno a;
+	//visitante v;
 	string dni, pass;
-	//int vuelve;
-	//int mode;
-	//usuario u;
-	//int opc;
-	/*
-	do{
-		if(menu_inicio(dni,pass)==1){
-			//Contraseña validada como alumno
-			mode=1;
-			vuelve=0;
-		}else if (menu_inicio(dni,pass)==2){
-			//Contraseña validada como administrador
-			mode =2;
-			vuelve=0;
-		}else if (menu_inicio(dni,pass)==0){
-			//Contraseña incorrecta
-		}else if (menu_inicio(dni,pass)==3){
-			mode=3;
-		}
-	}while(vuelve!=0);
-	enter();
-	//cout<<"Tu DNI es: "<<dni<<endl;
-	//cout<<"Tu pass es: "<<pass<<endl;
-	if(mode==2){
-		//menu_admin();
-		cout<<"ADMIN"<<endl;
-	}else if(mode ==3){
-		//menu_visitante();
-		cout<<"VISITANTE"<<endl;
-	}else{
-		//menu_alumno();
-		cout<<"ALUMNO"<<endl;
-	}
-*/
 
 		int opcion,modo,vuelve;
-		//string dni,pass;
-		//alumno a;
+		system(CLEAR);
 		do{
+			system(CLEAR);
 			cout<<"\t\t\t****Bienvenido al sistema, seleccione una opcion****\t\t\t\n\n"<<endl;
 			cout<<"1. Registrarme"<<endl;
 			cout<<"2. Iniciar sesion"<<endl;
@@ -83,9 +52,11 @@ int main() {
 			case 1:
 				fflush(stdin);
 				a.registro();
+				enter();
 				vuelve=1;
 				break;
 			case 2:
+				system(CLEAR);
 				fflush(stdin);
 				cout<<"Introduce el DNI: "<<endl;
 				cin>>dni;
@@ -118,98 +89,22 @@ int main() {
 
 		}while(opcion!=4 && vuelve==1);
 
-		//system("cls");
 		if(modo==2){
 				//menu_admin();
+				system(CLEAR);
 				menu_admin();
 				//cout<<"ADMIN"<<endl;
 			}else if(modo ==3){
 				//menu_visitante();
+				system(CLEAR);
 				menu_visitante();
 				//cout<<"VISITANTE"<<endl;
 			}else{
 				//menu_alumno();
+				system(CLEAR);
 				menu_alumno(dni);
 				//cout<<"ALUMNO"<<endl;
 			}
-
-		//TODO NO METE UN BARRA N AL METER EL PUTO USUARIO
-
-
-
-
-
-
-	/*
-	string x,y;
-	curso c(0);
-	alumno a("a","a","a","a","a");
-	alumno b;
-	int opc;
-	do{
-		system("clear");
-		cout<<"ELIGE UNA OPCION\n";
-		cout<<"0. Salir \n\n";
-		cout<<"1. Agregar curso\n\n";
-		cout<<"2. Listar cursos\n\n";
-		cout<<"3. Registro alumno\n\n";
-		cout<<"4. Iniciar sesion\n\n";
-		cout<<"5. Mis datos\n\n";
-		cout<<"6. Buscar curso\n\n";
-		cout<<"7. Inscribir curso\n\n";
-
-		cin>>opc;
-		//cin.get(opc);
-		switch(opc){
-		case 0:
-			break;
-		case 1:
-			fflush(stdin);
-			c.volcar_curso();
-			break;
-		case 2:
-			c.listarCursos();
-			break;
-		case 3:
-			fflush(stdin);
-			a.registro();
-			break;
-		case 4:
-			fflush(stdin);
-			a.inicio_sesion_bbdd();
-			break;
-		case 5:
-			fflush(stdin);
-			cout<<"Introduce el DNI de su perfil: "<<endl;
-			cin>>x;
-			a.Perfil(x);
-			fflush(stdin);
-			cout<<endl;
-			enter();
-			break;
-		case 6:
-			cout<<"Introduce el ID del curso que quieres buscar: ";
-			cin>>x;
-			if(c.buscar_curso(x)){
-				cout<<"ENCONTRADO!!"<<endl;
-			}else{
-				cout<<"NO ENCONTRADO!!"<<endl;
-			}
-			fflush(stdin);
-			enter();
-			break;
-		case 7:
-			fflush(stdin);
-			cout<<"Introduce tu DNI: ";
-			cin>>x;
-			cout<<endl;
-			fflush(stdin);
-			cout<<"Introduce el curso al que quieres registrarte: ";
-			cin>>y;
-			a.inscribir_curso(y, x); //IDCURSO, DNI
-		}
-
-	}while(opc!=0);*/
 
 	return 0;
 }
@@ -220,63 +115,15 @@ void enter(){
 	getchar();
 }
 
-
-int menu_inicio(string &dni, string &pass){
-	int opcion,res,vuelve;
-	//string dni,pass;
-	alumno a;
-	do{
-		cout<<"\t\t\t ***Bienvenido al sistema, seleccione una opción***\t\t\t\n\n"<<endl;
-		cout<<"1. Registrarme"<<endl;
-		cout<<"2. Iniciar sesion"<<endl;
-		cout<<"3. Entrar como visitante"<<endl;
-		cout<<"4. Salir del sistema"<<endl;
-		cin>>opcion;
-		switch(opcion){
-		case 1:
-			//fflsuh(stdin);
-			a.registro();
-			vuelve=1;
-			break;
-		case 2:
-			//fflush(stdin);
-			cout<<"Introduce el DNI: "<<endl;
-			cin>>dni;
-			//fflush(stdin);
-			cout<<"Introduce la contrasenaa: "<<endl;
-			cin>>pass;
-			if(dni=="root" && pass=="root"){
-				//MENU DE ADMINISTRADOR
-				res=2;
-				vuelve=1;
-			}else{
-				if(a.inicio_sesion_bbdd(dni,pass)){
-					res=1;
-					vuelve =1;
-				}else{
-					return 0;
-				}
-			}
-			break;
-		case 3:
-			res=3;
-			opcion=4;
-			break;
-		case 4:
-			break;
-		}
-
-	}while(opcion!=4);
-	return res;
-}
-
 void menu_admin(){
 
 	int opcion;
 	curso c;
 	alumno a;
+	system(CLEAR);
 	cout<<"\t\t\t***BIENVENIDO AL MENU DE ADMINISTRADOR***\t\t\t\n\n";
 	do{
+		system(CLEAR);
 		cout<<"\n1. Crear curso"<<endl;
 		cout<<"2. Listar cursos"<<endl;
 		cout<<"3. Crear usuario"<<endl;
@@ -308,8 +155,10 @@ void menu_admin(){
 void menu_visitante(){
 	int opc;
 	curso c;
+	system(CLEAR);
 	cout<<"\t\t\t***BIENVENIDO AL MENU DE VISITANTE***\t\t\t\n\n";
 	do{
+		system(CLEAR);
 		cout<<"\n1. Listar cursos."<<endl;
 		cout<<"2. Inscribir curso."<<endl;
 		cout<<"3. Salir"<<endl;
@@ -336,8 +185,10 @@ void menu_alumno(string &dni){
 	curso c;
 	alumno a;
 	string aux;
+	system(CLEAR);
 	cout<<"\t\t\t***BIENVENIDO AL MENU DE ALUMNO***\t\t\t\n\n";
 	do{
+		system(CLEAR);
 		cout<<"\n1. Listar cursos."<<endl;
 		cout<<"2. Inscribir curso."<<endl;
 		cout<<"3. Ver mi Perfil"<<endl;
@@ -348,22 +199,26 @@ void menu_alumno(string &dni){
 			cout<<"\nIntroduce una opcion valida(1-3)\n"<<endl;
 			break;
 		case 1:
+			system(CLEAR);
 			c.listarCursos();
 			break;
 		case 2:
+			system(CLEAR);
 			fflush(stdin);
 			cout<<"Introduce el ID del curso al que te quieres inscribir: "<<endl;
 			cin>>aux;
 			a.inscribir_curso(aux, dni);
 			break;
 		case 3:
+			fflush(stdin);
+			system(CLEAR);
 			a.Perfil(dni);
+			enter();
 			break;
 		case 4:
 			return;
 			break;
 		}
 	}while(opc!=4);
-
 }
 
